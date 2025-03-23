baf68928f9022ad39e2f84f24581e802fed900a8
Jorge Villar <jorge.vfg@gmail.com>
2025-03-23 20:29:24 +0100

    Implementación de funcionalidades e incorporación de assets en Public

--- app.js
+++ app.js
@@ -1,16 +1,21 @@
+import path from 'node:path'
 import express from 'express'
 import createError from 'http-errors'
 import logger from 'morgan'
+import { fileURLToPath } from 'url';
 import * as homeController from './controllers/homeController.js'
 
 const app = express()
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 app.set('views', 'views')
 app.set('view engine', 'ejs')
 app.locals.appName = 'nodePOP'
 
 app.use(logger('dev'))
-app.use(express.static())
+app.use(express.urlencoded({extended: false}))
+app.use(express.static(path.join(__dirname, 'public')))
 
 
 /* APP ROUTES */
